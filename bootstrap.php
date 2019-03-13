0faa905d96d98d6f0aebb5a88cba4b90932f984b
Anankke <anankke@protonmail.com>
2019-03-13 14:36:49 -0400

    fix bilibili and music163

--- bootstrap.php
+++ bootstrap.php
@@ -13,13 +13,116 @@ use s9e\TextFormatter\Configurator\Bundles\MediaPack;
 
 function subscribe(Dispatcher $events)
 {
-	$events->listen(
-		Configuring::class,
-		function (Configuring $event)
-		{
-			(new MediaPack)->configure($event->configurator);
-		}
-	);
-}
+    $events->listen(
+        Configuring::class,
+        function (Configuring $event)
+        {
+            $event->configurator->Autoimage;
+            $event->configurator->MediaEmbed->add(
+                'music163',
+                [
+                    'host'    => 'music.163.com',
+                    'extract' => '!music\\.163\\.com/#/(?\'mode\'song|album|playlist|dj)\\?id=(?\'id\'\\d+)!',
+                    'choose'  => [
+                        'when' => [
+                            [
+                                'test' => '@mode = \'album\'',
+                                'iframe'  => [
+                                    'width'  => 380,
+                                    'height' => 450,
+                                    'src'    => '//music.163.com/outchain/player?type=1&id={@id}&auto=0&height=450'
+                                ]
+                            ],
+                            [
+                                'test' => '@mode = \'song\'',
+                                'iframe'  => [
+                                    'width'  => 380,
+                                    'height' => 86,
+                                    'src'    => '//music.163.com/outchain/player?type=2&id={@id}&auto=0&height=66'
+                                ]
+                            ],
+                            [
+                                'test' => '@mode = \'dj\'',
+                                'iframe'  => [
+                                    'width'  => 380,
+                                    'height' => 86,
+                                    'src'    => '//music.163.com/outchain/player?type=3&id={@id}&auto=0&height=66'
+                                ]
+                            ]
+                        ],
+                        'otherwise' => [
+                            'iframe'  => [
+                                'width'  => 380,
+                                'height' => 450,
+                                'src'    => '//music.163.com/outchain/player?type=0&id={@id}&auto=0&height=450'
+                            ]
+                        ]
+                    ]
+                ]
+            );
+
+            $event->configurator->MediaEmbed->add(
+                'youku',
+                [
+                    'host'    => 'v.youku.com',
+                    'extract' => "!v\\.youku\\.com/v_show/\\id_(?'id'[-0-9A-Z_a-z]+)!",
+                    'iframe'  => [
+                        'width'  => 720,
+                        'height' => 405,
+                        'src'    => 'https://players.youku.com/embed/{@id}'
+                    ]
+                ]
+            );
+            $event->configurator->MediaEmbed->add(
+                'bilibili',
+                [
+                    'host'    => 'www.bilibili.com',
+                    'extract' => [
+                        "!www.bilibili.com/video/av(?'id'\\d+)/!",
+                        "!www.bilibili.com/mobile/video/av(?'id'\\d+)\\.html!"
+                    ],
+                    'iframe'  => [
+                        'width'  => 760,
+                        'height' => 450,
+                        'src'    => 'https://player.bilibili.com/player.html?aid={@id}'
+
+                    ]
+                ]
+            );
+            $event->configurator->MediaEmbed->add(
+                'open163',
+                [
+                    'host'    => 'open.163.com',
+                    'extract' => "!open\\.163\\.com/movie/[^/]+/[^/]+/[^/]+/[^/]+/(?'id'\\w+)\\.html!",
+                    'flash'  => [
+                        'width'  => 760,
+                        'height' => 450,
+                        'src'    => '//swf.ws.126.net/openplayer/v01/-0-2_{@id}-.swf'
+                    ]
+                ]
+            );
+            $event->configurator->MediaEmbed->add(
+                'qq',
+                [
+                    'host'    => 'qq.com',
+                    'extract' => [
+                        "!qq\\.com/x/cover/\\w+/(?'id'\\w+)\\.html!",
+                        "!qq\\.com/x/cover/\\w+\\.html\\?vid=(?'id'\\w+)!",
+                        "!qq\\.com/cover/[^/]+/\\w+/(?'id'\\w+)\\.html!",
+                        "!qq\\.com/cover/[^/]+/\\w+\\.html\\?vid=(?'id'\\w+)!",
+                        "!qq\\.com/x/page/(?'id'\\w+)\\.html!",
+                        "!qq\\.com/page/[^/]+/[^/]+/[^/]+/(?'id'\\w+)\\.html!"
+                    ],
+                    'iframe'  => [
+                        'width'  => 760,
+                        'height' => 450,
+                        'src'    => '//v.qq.com/iframe/player.html?vid={@id}&tiny=0&auto=0'
+                    ]
+                ]
+            );
+            (new MediaPack)->configure($event->configurator);
+        }
+    );
+};
 
 return __NAMESPACE__ . '\\subscribe';
